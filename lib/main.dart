8b34b8fe5be9b666e2ea68b8c98322cc78ee5403
John Doe <ayobamioyediran2018@yahoo.com>
2022-07-10 14:04:16 +0000

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:world_travel/screens/add_places_screen.dart';
 import '../screens/places_list_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
